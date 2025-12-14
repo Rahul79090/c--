cf781283dc943349fe945ad93b6aabfb2f25f277
Rahul79090 <kumarrahul032004@gmail.com>
2025-12-14 10:05:04 +0530

    Create game .cpp

---
+++ game .cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<" T i c k  C r o s s  G a m e "<<endl;
+    cout<<"Player1 [X] \nplayer2 [0]"<<endl;
+
+    cout<<"\t\t      |       |         \n ";
+    cout<<"\t\t  1   |   2   |    3    \n ";
+    cout<<"\t\t _____|_______|__________\n";
+    cout<<"\t\t      |       |         \n ";
+    cout<<"\t\t   4  |   5   |   6     \n ";
+    cout<<"\t\t _____|_______|__________\n";
+    cout<<"\t\t      |       |         \n ";
+    cout<<"\t\t   7  |   8   |   9     \n ";
+    cout<<"\t\t      |       |         \n ";
+    cout<<"\n\tPlayer1 [X] true : ";
+}
